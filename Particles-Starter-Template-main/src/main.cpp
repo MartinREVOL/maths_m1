3740008fccd0bf265cbf49c65a01a47ba9819dc2
Phylip3 <martin.revolbuisson@gmail.com>
2025-05-23 17:09:56 +0200

    final

--- Particles-Starter-Template-main/src/main.cpp
+++ Particles-Starter-Template-main/src/main.cpp
@@ -3,6 +3,8 @@
 #include <glm/glm.hpp>
 #include <vector>
 #include <glm/gtc/constants.hpp>
+#include <glm/gtx/color_space.hpp>
+#include <algorithm>
 #include <cmath>
 
 int main()
@@ -12,13 +14,17 @@ int main()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
-    // Structure de particule avec position aléatoire
     struct Particle
     {
         glm::vec2 position;
         glm::vec2 velocity;
         float mass;
-        float gravityForce = 1.f;
+        float gravityForce = 0.f;
+        float lifetime;
+        float age = 0.f;
+
+        glm::vec4 startColor;
+        glm::vec4 endColor;
 
         Particle()
         {
@@ -27,30 +33,48 @@ int main()
                 utils::rand(-1.f, 1.f)
             );
 
-            // Générer angle et vitesse aléatoires
-            float angle = utils::rand(0.f, glm::two_pi<float>()); // 0 à 2π
-            float speed = utils::rand(0.2f, 0.5f); // vitesses entre 0.2 et 0.5 unités/sec
-
+            float angle = utils::rand(0.f, glm::two_pi<float>());
+            float speed = utils::rand(0.1f, 0.3f);
             velocity = speed * glm::vec2(cos(angle), sin(angle));
 
             mass = utils::rand(0.1f, 0.3f);
+            lifetime = utils::rand(2.f, 12.f);
+
+            startColor = glm::vec4(utils::rand(0.f, 1.f), utils::rand(0.f, 1.f), utils::rand(0.f, 1.f), 1.f);
+            endColor = glm::vec4(utils::rand(0.f, 1.f), utils::rand(0.f, 1.f), utils::rand(0.f, 1.f), 1.f);
         }
 
-        void update(float dt, const glm::vec2& target, float k)
+        void update(float dt, /*const glm::vec2& target,*/ float k, float damping)
         {
             glm::vec2 gravity = glm::vec2(0.f, -gravityForce);
-            glm::vec2 springForce = -k * (position - target);
+            //glm::vec2 springForce = -k * (position - target);
+            glm::vec2 dampingForce = -damping * velocity;
 
-            glm::vec2 force = mass * gravity + springForce;
+            glm::vec2 force = mass * gravity + /*springForce +*/ dampingForce;
             glm::vec2 acceleration = force / mass;
 
             velocity += acceleration * dt;
             position += velocity * dt;
+
+            age += dt;
+        }
+
+        float current_radius() const
+        {
+            float x = glm::clamp((lifetime - age) / 2.f, 0.f, 1.f);
+            float bounce = std::abs(std::sin(10.f * x * glm::pi<float>()) * (1.f - x));
+            return 0.015f * x + 0.005f * bounce;
+        }
+
+        glm::vec4 current_color() const
+        {
+            float t = glm::clamp(age / lifetime, 0.f, 1.f);
+            float easedT = std::pow(t, 2.f);
+            return glm::mix(startColor, endColor, easedT);
         }
 
     };
 
-    // Création d'un tableau de particules
     std::vector<Particle> particles;
     for (int i = 0; i < 100; ++i)
     {
@@ -66,11 +90,20 @@ int main()
 
         glm::vec2 mouse = gl::mouse_position();
         float springStiffness = 0.5f;
+        float damping = 0.1f;
+
+        // Supprimer les particules mortes
+        particles.erase(
+            std::remove_if(particles.begin(), particles.end(), [](const Particle& p) {
+                return p.age > p.lifetime;
+            }),
+            particles.end()
+        );
 
         for (Particle& p : particles)
         {
-            p.update(dt, mouse, springStiffness);
-            utils::draw_disk(p.position, 0.01f, glm::vec4(1.f, 1.f, 1.f, 1.f));
+            p.update(dt, /*mouse,*/ springStiffness, damping);
+            utils::draw_disk(p.position, p.current_radius(), p.current_color());
         }
     }
 }
